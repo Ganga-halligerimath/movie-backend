4cddc5498ee297fb5676f938a1c9b45b6f3ad63d
Ganga <gangadevihalligerimath@gmail.com>
2026-01-29 19:13:31 +0530

    Fix Railway port 8080 binding

--- src/server.js
+++ src/server.js
@@ -11,9 +11,10 @@ console.log("JWT_SECRET 👉", process.env.JWT_SECRET);
 connectDB();
 
 // const PORT = process.env.PORT || 5000;
-const PORT = process.env.PORT;
-
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
   console.log(`🚀 Server running on port ${PORT}`);
 });
+
+
