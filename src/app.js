2bc16e44f630c4312f6d2c554e5f9138f8cfde97
Ganga <gangadevihalligerimath@gmail.com>
2026-01-29 23:31:06 +0530

    Update Axios config for   backend

--- src/app.js
+++ src/app.js
@@ -1,19 +1,57 @@
+// // src/app.js
+// import express from "express";
+// import cors from "cors";
+
+// // ⚠ Make sure to include .js for local files
+// import errorHandler from "./middleware/errorMiddleware.js";  
+// import authRoutes from "./routes/authRoutes.js";
+// import movieRoutes from "./routes/movieRoutes.js";
+// import movieQueue from "./queue/movieQueue.js";
+// import adminRoutes from "./routes/adminRoutes.js"
+
+// const app = express();
+
+// app.use(cors({
+//   origin: ["http://localhost:3000",
+//      "https://moive-nwzn.vercel.app"],
+//   credentials: true
+// }))
+// app.use(express.json());
+
+// // Routes
+// app.use("/api/auth", authRoutes);
+// app.use("/api/movies", movieRoutes);
+// app.use("/api/admin", adminRoutes);
+
+// // Queue
+// movieQueue();
+
+// // Error middleware must come last
+// app.use(errorHandler);
+
+// export default app;   // ES Modules export
+
+
+
 // src/app.js
 import express from "express";
 import cors from "cors";
 
-// ⚠ Make sure to include .js for local files
 import errorHandler from "./middleware/errorMiddleware.js";  
 import authRoutes from "./routes/authRoutes.js";
 import movieRoutes from "./routes/movieRoutes.js";
 import movieQueue from "./queue/movieQueue.js";
-import adminRoutes from "./routes/adminRoutes.js"
+import adminRoutes from "./routes/adminRoutes.js";
 
 const app = express();
 
+// CORS: allow localhost and Vercel frontend
 app.use(cors({
-    origin: "*",
-    credentials: true
+  origin: [
+    "http://localhost:3000",
+    "https://moive-ebus.vercel.app/"
+  ],
+  credentials: true
 }));
 
 app.use(express.json());
@@ -26,7 +64,7 @@ app.use("/api/admin", adminRoutes);
 // Queue
 movieQueue();
 
-// Error middleware must come last
+// Error middleware
 app.use(errorHandler);
 
-export default app;   // ES Modules export
+export default app;
